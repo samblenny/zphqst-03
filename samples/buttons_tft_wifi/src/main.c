9da2a1265a72d9c7cc149bd6d448f4c483044a66
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-03-07 03:55:54 +0000

    getting started using display

--- samples/buttons_tft_wifi/src/main.c
+++ samples/buttons_tft_wifi/src/main.c
@@ -1,6 +1,23 @@
-/* SPDX-FileCopyrightText: Copyright 2025 Sam Blenny */
-/* SPDX-License-Identifier: Apache-2.0 OR MIT */
+/*
+ * SPDX-FileCopyrightText: Copyright 2025 Sam Blenny
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ */
 
-/* Build shell from zephyr/samples/subsys/shell/shell_module/
-*/
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+
+#define WIDTH (DT_PROP(DT_CHOSEN(zephyr_display), width))
+#define HEIGHT (DT_PROP(DT_CHOSEN(zephyr_display), height))
+static const struct device *const display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+
+int main(void) {
+	printf("display: %s, %dx%d\n", display->name, WIDTH, HEIGHT);
+	while(1) {
+		k_msleep(16);
+	}
+}
