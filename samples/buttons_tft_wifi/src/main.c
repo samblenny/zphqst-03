97c3e901e6901b6d0b7b927f28e6c0b65bfc17f2
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-03-08 02:54:49 +0000

    get LVGL button click working
    This finally connects all the necessary Kconfig, devicetree, and C
    code plumbing so that pressing the Feather TFT's physical Boot
    button causes LVGL's onscreen button to be pressed.

--- samples/buttons_tft_wifi/src/main.c
+++ samples/buttons_tft_wifi/src/main.c
@@ -34,19 +34,27 @@ int main(void) {
 	lv_init();
 	lv_tick_set_cb(k_uptime_get_32);
 	const struct device *display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	const struct device *keypad =
+		DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_keypad_input));
 
 	// Change background color (see also lv_palette_main())
 	lv_obj_t *scr = lv_screen_active();
 	lv_color_t bg = lv_palette_darken(LV_PALETTE_GREY, 4);
 	lv_obj_set_style_bg_color(scr, bg, 0);
 
-	// Make widgets
+	// Make widgets, adding to group to enable keypad control
 	lv_obj_t *btn1 = lv_button_create(lv_screen_active());
 	lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 0);
-	lv_obj_add_event_cb(btn1, btn1_callback, LV_EVENT_CLICKED, NULL);
 	lv_obj_add_event_cb(btn1, btn1_callback, LV_EVENT_PRESSED, NULL);
 	lv_obj_t *label = lv_label_create(btn1);
 	lv_label_set_text(label, "Button 1");
+
+	// Configure a group so keypad can control focus and button pressing.
+	// This expects that the devicetree config will provide keys that are
+	// mapped to LV_KEY_LEFT, LV_KEY_RIGHT, and LV_KEY_ENTER.
+	lv_group_t *grp = lv_group_create();
+	lv_group_add_obj(grp, btn1);
+	lv_indev_set_group(lvgl_input_get_indev(keypad), grp);
 
 	// Event loop
 	lv_timer_handler();
