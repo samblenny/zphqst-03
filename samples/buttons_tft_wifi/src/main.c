5d5ec0fc42563b1bb392d51f9e3fac68d6a2e737
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-03-08 04:05:19 +0000

    add wifi status callback

--- samples/buttons_tft_wifi/src/main.c
+++ samples/buttons_tft_wifi/src/main.c
@@ -11,13 +11,17 @@
  * https://docs.lvgl.io/9.2/porting/timer_handler.html
  * https://docs.lvgl.io/9.2/overview/display.html  (change bg color)
  * https://docs.lvgl.io/9.2/overview/color.html  (color constants)
+ * https://docs.zephyrproject.org/latest/doxygen/html/group__net__mgmt.html
+ * https://docs.zephyrproject.org/latest/doxygen/html/structnet__mgmt__event__callback.html
+ * https://github.com/zephyrproject-rtos/zephyr/blob/main/subsys/net/l2/wifi/wifi_shell.c
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/display.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/shell/shell.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
@@ -29,6 +33,18 @@ static void btn1_callback(lv_event_t *event) {
 	printk("button clicked\n");
 }
 
+static void net_callback(struct net_mgmt_event_callback *cb,
+	uint32_t mgmt_event, struct net_if *iface)
+{
+	if(mgmt_event == NET_EVENT_WIFI_CONNECT_RESULT) {
+		printk("net: WIFI_CONNECT\n");
+	} else if(mgmt_event == NET_EVENT_WIFI_DISCONNECT_RESULT) {
+		printk("net: WIFI_DISCONNECT\n");
+	} else {
+		printk("net: unknown event\n");
+	}
+}
+
 int main(void) {
 	// Inits
 	lv_init();
@@ -36,13 +52,14 @@ int main(void) {
 	const struct device *display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	const struct device *keypad =
 		DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_keypad_input));
+	struct net_mgmt_event_callback net_status;
 
 	// Change background color (see also lv_palette_main())
 	lv_obj_t *scr = lv_screen_active();
 	lv_color_t bg = lv_palette_darken(LV_PALETTE_GREY, 4);
 	lv_obj_set_style_bg_color(scr, bg, 0);
 
-	// Make widgets, adding to group to enable keypad control
+	// Make widgets and align them on the screen
 	lv_obj_t *btn1 = lv_button_create(lv_screen_active());
 	lv_obj_align(btn1, LV_ALIGN_CENTER, 0, 0);
 	lv_obj_add_event_cb(btn1, btn1_callback, LV_EVENT_PRESSED, NULL);
@@ -50,11 +67,17 @@ int main(void) {
 	lv_label_set_text(label, "Button 1");
 
 	// Configure a group so keypad can control focus and button pressing.
-	// This expects that the devicetree config will provide keys that are
-	// mapped to LV_KEY_LEFT, LV_KEY_RIGHT, and LV_KEY_ENTER.
+	// This expects that the devicetree config has provided a keypad with
+	// pysical buttons mapped to navigation keys such as LV_KEY_LEFT,
+	// LV_KEY_RIGHT, and LV_KEY_ENTER. Minimum is LV_KEY_ENTER.
 	lv_group_t *grp = lv_group_create();
 	lv_group_add_obj(grp, btn1);
 	lv_indev_set_group(lvgl_input_get_indev(keypad), grp);
+
+	// Register to get updates about wifi connection status
+	net_mgmt_init_event_callback(&net_status, net_callback,
+		NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT);
+	net_mgmt_add_event_callback(&net_status);
 
 	// Event loop
 	lv_timer_handler();
